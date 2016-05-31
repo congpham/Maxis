f17a972a4c8ea6ccfbd5ec7f219b88f5459139ea
congpham <arronramsey421@gmail.com>
2016-05-31 21:47:32 +0700

    [Initial]

---
+++ Maxis/Maxis.Artemis.Misc/Misc.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Maxis.Artemis.Misc
+{
+    public class Misc
+    {
+        public int UId { get; set; }
+    }
+}
